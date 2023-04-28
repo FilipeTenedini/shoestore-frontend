ad97c7bbc32dff2d112038cf97553bdb57cc242d
Filipe Tenedini <105571583+FilipeTenedini@users.noreply.github.com>
2023-04-28 13:38:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { ResetCss } from './styles/resetCss';
+import { GlobalStyle } from './styles/GlobalStyle';
+import MyRoutes from './Routes/MyRoutes';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <ResetCss />
+    <GlobalStyle />
+    <MyRoutes />
+   </>
   );
 }
-
-export default App;
