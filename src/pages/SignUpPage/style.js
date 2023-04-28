249fbd957da25a05d45bbe0cf1df3bbe9ad1b4a5
Diego Beker <diegobecker2007@hotmail.com>
2023-04-28 18:01:11 -0300

    refactor: removing styles from globalStyle

--- src/pages/SignUpPage/style.js
+++ src/pages/SignUpPage/style.js
@@ -19,9 +19,24 @@ export const SignUpContainer = styled.section`
         font-size: 16px;
         margin: 40px 0;
     }
+    form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 15px;
+        width: 100%;
+    }
     input{
         background: #1C1C1C;
         color: #FFFFFF;
+        font-size: 20px;
+        width: calc(100% - 30px);
+        outline: none;
+        border: 1px solid #FFFFFF;
+        border-radius: 24px;
+        padding: 15px;
+        margin: 1px;
     }
     button{
         display: flex;
@@ -32,9 +47,14 @@ export const SignUpContainer = styled.section`
         color: #1C1C1C;
         font-weight: 500;
         font-size: 16px;
+        border-radius: 24px;
+        padding: 12px;
     }
     a{
         margin: 50px 0;
         text-decoration: none;
+    }
+    a:-webkit-any-link {
+      color: #FFFFFF;
     }
 `;
